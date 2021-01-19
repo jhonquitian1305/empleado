93a833e2fbf66f04e7c4278042404459af9f16aa
Jhon Quitian <jhon.quitian@udea.edu.co>
2021-01-19 18:33:34 -0500

    Subiendo el proyecto

--- src/main/java/com/udea/empleado/dao/IPersonaDAO.java
+++
@@ -1,7 +0,0 @@
-package com.udea.empleado.dao;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import com.udea.empleado.model.Persona;
-
-@Repository
-public interface IPersonaDAO extends CrudRepository<Persona, Long> {}
